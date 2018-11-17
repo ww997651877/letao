237da2af2b9768952036f6c2b17c2eee53582378
Coco <997651877@qq.com>
2018-11-17 15:22:23 +0800

    基本列表渲染完成，文件上传功能

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -42,6 +42,48 @@ $(function() {
     // 显示添加模态框
     $('#addModal').modal('show');
     
+    // 发送ajax请求，获取下拉菜单的列表数据（全部的一级分类）
+    // 通过分页获取一级分类的接口，模拟获取全部数据的接口，page=1,pageSize:100
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: 1,
+        pageSize: 100
+      },
+      dataType: 'json',
+      success: function(info) {
+        console.log(info);
+        var htmlStr = template('dropdownTpl', info);
+        $('.dropdown-menu').html(htmlStr);
+      }
+    })
+
+  });
+
+  // 3.给下拉菜单的所有a添加点击事件，通过事件委托注册
+  $('.dropdown-menu').on('click','a',function() {
+    // 获取a的文本
+    var txt = $(this).text();
+
+    // 将文本设置给按钮
+    $('#dropdownText').text( txt );
+    
+  });
+
+  // 4.进行文件上传初始化
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    // 表示文件上传完成的回调函数
+    done: function( e, data ) {
+      console.log(data);
+      // 后台返回的结果
+      var result = data.result;
+      // 获取文件上传的地址
+      var picUrl = result.picAddr;
+      // 设置给img的src
+      $('#imgBox img').attr('src', picUrl);
+    }
   })
 
 })
