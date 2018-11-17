81dae77011b87613e5e4dd118693f0946be7a9da
Coco <997651877@qq.com>
2018-11-17 11:44:56 +0800

    二级分类-准备模态框完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -1,5 +1,5 @@
 $(function() {
-  // 一进入页面，发送请求，获取数据进行渲染
+  // 1.一进入页面，发送请求，获取数据进行渲染
   var currentPage = 1; //当前页
   var pageSize = 5; //每页条数
 
@@ -20,7 +20,28 @@ $(function() {
         // 根据模板引擎进行渲染
         var htmlStr = template("secondTpl",info);
         $('tbody').html(htmlStr);
+
+        // 进行分页初始化
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3, //版本号
+          totalPages: Math.ceil( info.total / info.size ),
+          currentPage: info.page,
+          onPageClicked: function( a, b, c, page) {
+            // 更新当前页
+            currentPage = page;
+            // 重新渲染
+            render();
+          }
+        })
       }
     })
   }
+
+  // 2.点击添加按钮，显示添加模态框
+  $('#addBtn').click(function() {
+    // 显示添加模态框
+    $('#addModal').modal('show');
+    
+  })
+
 })
