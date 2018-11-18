6bad74c11bdd3010c11b31bb92a3f42384c28466
Coco <997651877@qq.com>
2018-11-18 11:24:54 +0800

    back2一级分类开发完成

--- public/back2/js/first.js
+++ public/back2/js/first.js
@@ -2,7 +2,7 @@ $(function() {
   
   var currentPage = 1;
   var pageSize = 5;
-  // 一进入页面，发送ajax请求，获取数据，进行渲染
+  // 1.一进入页面，发送ajax请求，获取数据，进行渲染
   render();
   function render() {
     $.ajax({
@@ -32,4 +32,57 @@ $(function() {
     }
     });
   }
+
+  // 2.点击添加按钮，显示添加模态框
+  $('#addBtn').click(function() {
+    $('#addModal').modal('show');
+  });
+
+
+  // 3.表单校验功能
+  $('#form').bootstrapValidator({
+    // 配置校验图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+    // 字段列表
+    fields: {
+      categoryName: {
+        // 校验规则
+        validators: {
+          // 非空
+          notEmpty: {
+            message: "请输入一级分类"
+          }
+        }
+      }
+    }
+  });
+
+  // 4.注册表单校验成功事件
+  $('#form').on('success.form.bv',function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/category/addTopCategory',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function(info) {
+        console.log(info);
+        if (info.success) {
+          $('#addModal').modal('hide');
+          currentPage = 1;
+          render();
+
+          $('#form').data('bootstrapValidator').resetForm(true);
+        }
+      }
+    });
+  });
+
+
+
+
 })
